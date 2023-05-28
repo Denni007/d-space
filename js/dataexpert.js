f1389104ecaef14ad6111fe498c4bb79b696eb6b
Denni007 <kukadiyadenish@gmail.com>
2023-05-28 18:29:21 +0530

    services

--- js/dataexpert.js
+++ js/dataexpert.js
@@ -274,7 +274,11 @@
     dspaceexpert.pageviews.add()) : dspaceexpert.create();
     window.dspaceexpert = dspaceexpert;
 })(window);
-
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WVWQ985');
 
 
 var urlcheck = /utm|gclid|fbp|fbc/;
