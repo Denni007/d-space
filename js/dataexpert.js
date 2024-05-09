b29d15ce668678c071203110d797fa3846b8c543
Denish Patel <denishpatel@Apples-MacBook-Air.local>
2024-05-10 02:18:51 +0530

    our team

--- js/dataexpert.js
+++ js/dataexpert.js
@@ -372,7 +372,7 @@ urlcheck.test(document.location.href) ? (dspaceexpert.visits.create(),
   totalpageviews2.innerHTML += data.pageviews;
  }
  
-if(location.hostname == "www.starluxetech"){
+if(location.hostname.includes("starluxetech")){
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
